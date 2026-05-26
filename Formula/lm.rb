34d2c2b468b2225e5cc940a5e59b0220d3c10445
bagaspra16 <bagaspratamajunianika72@gmail.com>
2026-05-26 07:17:54 +0700

    formula: v0.4.1

--- Formula/lm.rb
+++ Formula/lm.rb
@@ -1,8 +1,8 @@
 class Lm < Formula
   desc "Developer-aware storage analysis & cleanup for macOS (terminal-first)"
   homepage "https://github.com/bagaspra16/lean-mac"
-  url "https://github.com/bagaspra16/lean-mac/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "fe94e7366f38a7e6b769725eb3112ee1535dc98517c4fbf89736a3b625484034"
+  url "https://github.com/bagaspra16/lean-mac/archive/refs/tags/v0.4.1.tar.gz"
+  sha256 "6d6604d59b6e3dfef884a341e505b01d5911c62c56249ee02082382f06bc9994"
   license "MIT"
   head "https://github.com/bagaspra16/lean-mac.git", branch: "main"
 
